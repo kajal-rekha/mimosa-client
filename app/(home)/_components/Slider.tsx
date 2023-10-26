5c7b0d2e00d4770311ec7220de0353ef49209346
kajal-2011 <kajalrekha888@gmail.com>
2023-10-26 08:09:44 +0600

    data added

--- app/(home)/_components/Slider.tsx
+++ app/(home)/_components/Slider.tsx
@@ -1,5 +1,32 @@
+'use client';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css/pagination';
+
 const Slider = () => {
-  return <section className='h-[calc(100vh-5rem)]'>Slider</section>;
+  return (
+    <section className='h-[calc(100vh-5rem)]'>
+      <Swiper
+        pagination={{ clickable: true }}
+        navigation={true}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        grabCursor={true}
+        speed={500}
+        modules={[Navigation, Pagination, Autoplay]}
+        className='mySwiper h-full w-full'
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+      </Swiper>
+    </section>
+  );
 };
 
 export default Slider;
---
+++ data/sliderContents.ts
@@ -0,0 +1,37 @@
+export const data = [
+  {
+    id: 1,
+    image: 'massage_chair_image.jpg',
+    heading: 'Ultimate Relaxation Zone',
+    subHeading:
+      'Immerse yourself in the epitome of tranquility as you surrender to the gentle embrace of our state-of-the-art massage chairs. Let the rhythmic movements and expertly designed technology dissolve the stresses of the day, leaving you in a state of pure, blissful serenity.',
+  },
+  {
+    id: 2,
+    image: 'aromatherapy_diffuser_image.jpg',
+    heading: 'Scented Serenity',
+    subHeading:
+      'Embark on a sensory journey that transcends ordinary relaxation. Elevate your spa experience with aromatherapy, where each breath becomes a moment of rejuvenation. Breathe, relax, and unwind as carefully curated scents envelop you, creating an atmosphere of pure tranquility for a truly transformative escape.',
+  },
+  {
+    id: 3,
+    image: 'led_skincare_lamp_image.jpg',
+    heading: 'Illuminate Your Beauty',
+    subHeading:
+      'Step into a realm where your inner radiance takes center stage. Revitalize your skin and unveil a luminous glow with the power of cutting-edge LED technology. Our skincare lamps are designed to nourish and rejuvenate, leaving you with a complexion that not only reflects light but radiates confidence and beauty.',
+  },
+  {
+    id: 4,
+    image: 'hot_stone_massage_set_image.jpg',
+    heading: 'Healing Heat Therapy',
+    subHeading:
+      'Immerse yourself in the therapeutic embrace of our hot stone massage set—a luxurious journey of relaxation for both body and soul. The harmonious combination of soothing warmth and expertly placed stones creates a transformative experience, melting away tension and restoring balance to your senses.',
+  },
+  {
+    id: 5,
+    image: 'pedi_spa_station_image.jpg',
+    heading: 'Pamper Your Feet',
+    subHeading:
+      'Indulge in the ultimate luxury for your feet with our pedi-spa station. Step into a world of relaxation and rejuvenation as your feet are pampered with care and precision. Our expert therapists will ensure that every step you take is one of comfort and that your feet emerge not just happy but revitalized and ready to conquer the world.',
+  },
+];
