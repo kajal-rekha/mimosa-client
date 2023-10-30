999e0581010d5e6e57c2fb5430b4f273a2d7bf9d
kajal-2011 <kajalrekha888@gmail.com>
2023-10-30 16:26:16 +0600

    beauty package card added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['res.cloudinary.com', 'images.pexels.com', 'hips.hearstapps.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
