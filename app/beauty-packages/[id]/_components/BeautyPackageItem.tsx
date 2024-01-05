c71dbe5fd47fdaf1b8e26a0f2631682eeb1912cd
kajal-2011 <kajalrekha888@gmail.com>
2024-01-05 17:06:53 +0600

    beauty package item added

--- app/beauty-packages/[id]/_components/BeautyPackageItem.tsx
+++ app/beauty-packages/[id]/_components/BeautyPackageItem.tsx
@@ -1,11 +1,25 @@
 import { beautyPackageType } from '@/types/beautyPackage';
+import Image from 'next/image';
+import { useState } from 'react';
+import BeautyPAcckageItemContent from './BeautyPAcckageItemContent';
+import BeautyPackageItemImage from './BeautyPackageItemImage';
 
 interface beautyPackageItemProps {
   item: beautyPackageType;
 }
 
 const BeautyPackageItem: React.FC<beautyPackageItemProps> = ({ item }) => {
-  return <section className='sp container'>{item.title}</section>;
+  return (
+    <section className='sp grid-cols container grid gap-20 md:grid-cols-2 mt-20'>
+      <div>
+        <BeautyPackageItemImage images={item.images}/>
+      </div>
+
+      <div>
+        <BeautyPAcckageItemContent item={item} />
+      </div>
+    </section>
+  );
 };
 
 export default BeautyPackageItem;
---
+++ app/beauty-packages/[id]/_components/BeautyPackageItemImage.tsx
@@ -0,0 +1,50 @@
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import { useState } from 'react';
+
+interface beautyPackageItemImageProps {
+  images: string[];
+}
+
+const BeautyPackageItemImage: React.FC<beautyPackageItemImageProps> = ({
+  images,
+}) => {
+  const [selectedImage, setSelectedImage] = useState<string>(images[0]);
+
+  return (
+    <div className='flex  gap-5'>
+      <div className='flex flex-col items-center justify-center gap-5'>
+        {images?.map((image, i) => (
+          <div key={i}>
+            <div className='h-20 w-20 overflow-hidden'>
+              <Image
+                src={image}
+                alt='beauty package image'
+                width={750}
+                height={750}
+                priority
+                onClick={() => setSelectedImage(image)}
+                className={cn(
+                  'h-full w-full object-cover',
+                  image === selectedImage ? 'border border-blue' : ''
+                )}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className='h-[550px] w-full overflow-hidden'>
+        <Image
+          src={selectedImage}
+          alt='beauty package image'
+          width={780}
+          height={780}
+          priority
+          className='eq h-full w-full object-cover'
+        />
+      </div>
+    </div>
+  );
+};
+
+export default BeautyPackageItemImage;
